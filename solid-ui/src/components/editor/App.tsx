5436a18ca99f51ec79a68ce8d0f5ff0378adaa6c
Birk Skyum <birk.skyum@pm.me>
2023-09-27 03:25:55 +0200

    real data in talbe

--- solid-ui/src/components/editor/App.tsx
+++ solid-ui/src/components/editor/App.tsx
@@ -1,100 +1,121 @@
-import {ICellEditorParams, ICellEditor, ModuleRegistry} from '@ag-grid-community/core';
-import {Component, createResource, onMount} from 'solid-js';
-import {createEffect, createSignal} from "solid-js";
-import AgGridSolid, {AgGridSolidRef} from '@ag-grid-community/solid';
-import {ClientSideRowModelModule} from '@ag-grid-community/client-side-row-model';
+import {
+  ICellEditorParams,
+  ICellEditor,
+  ModuleRegistry,
+} from "@ag-grid-community/core";
+import { Component, createResource, onMount } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
+import AgGridSolid, { AgGridSolidRef } from "@ag-grid-community/solid";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
-import '@ag-grid-community/styles/ag-grid.css';
+import "@ag-grid-community/styles/ag-grid.css";
 import "@ag-grid-community/styles/ag-theme-alpine.css";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 
 export const MySolidEditor = (props: ICellEditorParams) => {
-    let value = props.value;
-    let refInput: any;
+  let value = props.value;
+  let refInput: any;
 
-    const api: ICellEditor = {
-        getValue: () => value
-    };
+  const api: ICellEditor = {
+    getValue: () => value,
+  };
 
-    (props as any).ref(api);
+  (props as any).ref(api);
 
-    const onValueChanged = (event: any) => {
-        value = Number(event.target.value);
-    };
+  const onValueChanged = (event: any) => {
+    value = Number(event.target.value);
+  };
 
-    createEffect(() => {
-        refInput.focus();
-    })
+  createEffect(() => {
+    refInput.focus();
+  });
 
-
-    return (
-        <input type="number" class={styles["my-editor"]}
-               ref={refInput}
-               value={value}
-               onChange={onValueChanged}
-        />    );
-}
+  return (
+    <input
+      type="number"
+      class={styles["my-editor"]}
+      ref={refInput}
+      value={value}
+      onChange={onValueChanged}
+    />
+  );
+};
 
 const fetchData = async () => {
+  console.log("Try fetch");
 
-    console.log("Try fetch")
+  let response = await fetch(
+    `https://www.ag-grid.com/example-assets/olympic-winners.json`
+  );
 
-    let response = await fetch(`https://www.ag-grid.com/example-assets/olympic-winners.json`)
-    
-    
-    console.log('response', response)
-    let data = await response.json()
-    console.log('data', data)
+  console.log("response", response);
+  let data = await response.json();
+  console.log("data", data);
 
-    return data;
-}
+  return data;
+};
 const MyEditor: Component = () => {
-    
-    const [rowData] = createResource<any[]>(fetchData);
+//   const [rowData] = createResource<any[]>(fetchData);
 
-    const columnDefs = [
-        {
-            field: 'country'
-        },
-        {
-            field: 'athlete',
-        },
-        {
-            field: 'gold',
-            editable: true,
-            cellEditor: MySolidEditor
-        },
-        {
-            field: 'silver',
-            cellEditor: MySolidEditor,
-            cellEditorPopup: true
-        }
-    ];
+  const rowData = [
+    { use: "Diesel", annual_consumption: 0.0, unit: "L" },
+    { use: "Petrol/Gasoline, regular", annual_consumption: 0.0, unit: "L" },
+    {
+      use: "Propane gas / butane gas (bottle, tank)",
+      annual_consumption: 0.0,
+      unit: "kg",
+    },
+    { use: "Natural gas", annual_consumption: 0.0, unit: "m3" },
+    { use: "Coal", annual_consumption: 0.0, unit: "kg" },
+    { use: "Wood", annual_consumption: 0.0, unit: "tonnes" },
+    { use: "Biofuels", annual_consumption: 0.0, unit: "L" },
+    { use: "Biogas", annual_consumption: 0.0, unit: "m3 CH4" },
+    { use: "Irrigation Water", annual_consumption: 0.0, unit: "m3" },
+    { use: "Drinking Water", annual_consumption: 0.0, unit: "m3" },
+  ];
 
-    const defaultColDef = {
-        resizable: true,
-        filter: true,
-        editable: true,
-        sortable: true,
-        flex: 1
-    };
+  const columnDefs = [
+    {
+      field: "use",
+      editable: false,
+    },
+    {
+      field: "annual_consumption",
+      editable: true,
+      cellEditor: MySolidEditor,
+    },
+    {
+      field: "unit",
+      editable: false,
+    },
+  ];
 
-    let gridRef: AgGridSolidRef;
+  const defaultColDef = {
+    resizable: true,
+    filter: true,
+    editable: true,
+    sortable: true,
+    flex: 1,
+  };
 
-    return (
-        <div style={{height: '400px', display: 'flex', "flex-direction": 'column'}}>
-            <div class="ag-theme-alpine" style={{"flex-grow": 1}}>
-                <AgGridSolid
-                    columnDefs={columnDefs}
-                    rowData={rowData()}
-                    defaultColDef={defaultColDef}
-                    ref={gridRef!}
-                />
-            </div>
-        </div>
-    );
+  let gridRef: AgGridSolidRef;
+
+  return (
+    <div
+      style={{ height: "400px", display: "flex", "flex-direction": "column" }}
+    >
+      <div class="ag-theme-alpine" style={{ "flex-grow": 1 }}>
+        <AgGridSolid
+          columnDefs={columnDefs}
+          rowData={rowData}
+          defaultColDef={defaultColDef}
+          ref={gridRef!}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MyEditor;
