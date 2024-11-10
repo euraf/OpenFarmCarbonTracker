24f1827ec3a6bfbfc04a536d7d1964363297d46a
Birk Skyum <birk.skyum@pm.me>
2024-11-10 02:23:37 +0100

    pie chart

--- ui/src/components/chart.tsx
+++ ui/src/components/chart.tsx
@@ -1,7 +1,8 @@
 import { createMemo } from "solid-js";
-import { LineChart } from "~/components/ui/charts";
+import { LineChart, PieChart } from "~/components/ui/charts";
 import { store } from "~/store/store";
 import { calculateBuildingAndEquipmentEmission } from "~/util/buildingAndEquipmentEmission";
+import { calculateFuelEmission } from "~/util/emission";
 
 export const MyChart = (props: {
   data: { accumulated: number[]; contribution: number[] };
@@ -30,28 +31,78 @@ export const MyChart = (props: {
     };
   });
 
+  const pieChartData = createMemo(() => {
+    const totalLandUseEmission = props.data.accumulated.reduce((sum, value) => sum + value, 0);
+    const buildingAndEquipmentEmission = calculateBuildingAndEquipmentEmission();
+    const totalBuildingAndEquipmentEmission = buildingAndEquipmentEmission.accumulated.reduce((sum, value) => sum + value, 0);
+    const totalFuelEmission = calculateFuelEmission() * props.data.accumulated.length;
+
+    return {
+      labels: ["Land Use", "Buildings/Equipment", "Energy/Fuel"],
+      datasets: [
+        {
+          data: [totalLandUseEmission, totalBuildingAndEquipmentEmission, totalFuelEmission],
+          backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        },
+      ],
+    };
+  });
+
   return (
-    <div>
-      <LineChart
-        data={chartData()}
-        options={{
-          responsive: true,
-          maintainAspectRatio: false,
-          animation: {
-            duration: 0, // disable animations
-          },
-          scales: {
-            y: {
-              title: {
-                display: true,
-                text: "kg CO2e",
+    <div class="chart-container">
+      <div class="chart">
+        <LineChart
+          data={chartData()}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            animation: {
+              duration: 0, // disable animations
+            },
+            scales: {
+              y: {
+                title: {
+                  display: true,
+                  text: "kg CO2e",
+                },
               },
             },
-          },
-        }}
-        width={500}
-        height={500}
-      />
+          }}
+          width={500}
+          height={500}
+        />
+      </div>
+      <div class="chart">
+        <PieChart
+          data={pieChartData()}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            animation: {
+              duration: 0, // disable animations
+            },
+          }}
+          width={500}
+          height={500}
+        />
+      </div>
+      <style jsx>{`
+        .chart-container {
+          display: flex;
+          flex-direction: column;
+        }
+        .chart {
+          height: 500px;
+        }
+        @media (min-width: 800px) {
+          .chart-container {
+            flex-direction: row;
+          }
+          .chart {
+            flex: 1;
+          }
+        }
+      `}</style>
     </div>
   );
 };
