25d9ce25450acaabe777732fd5280bbc87badd31
Birk Skyum <birk.skyum@pm.me>
2025-01-10 21:50:57 +0100

    readme

--- carbon-tracker/sensor.py
+++
@@ -1,5 +0,0 @@
-import random, time
-while True:
-    time.sleep(random.random() * 5)  # wait 0 to 5 seconds
-    temperature = (random.random() * 20) - 5  # -5 to 15
-    print(temperature, flush=True, end='')
